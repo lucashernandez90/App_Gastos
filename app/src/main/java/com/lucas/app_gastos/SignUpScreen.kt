4077e75ac87974873d718b5fb21f22c2b6b92e2a
Wanessa Koserski <wkoserski@gmail.com>
2024-05-28 23:05:28 -0300

    feat: add firebase connection

--- app/src/main/java/com/lucas/app_gastos/SignUpScreen.kt
+++ app/src/main/java/com/lucas/app_gastos/SignUpScreen.kt
@@ -1,22 +1,29 @@
 package com.lucas.app_gastos
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
 import com.lucas.app_gastos.databinding.ActivitySignUpScreenBinding
 
 
 class SignUpScreen : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpScreenBinding
+
+    private lateinit var firebase: Firebase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebase = Firebase
 
         binding.btnCreateLogin.setOnClickListener{
             openMainActivity()
@@ -27,4 +34,5 @@ class SignUpScreen : AppCompatActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
+
 }
